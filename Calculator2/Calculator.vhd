d601506f39fd2e5c5e1df8db56a840ea30164dca
Soheil <sohail_nader@yahoo.com>
2023-01-31 22:36:51 +0330

    Update Calculator.vhd

--- Calculator2/Calculator.vhd
+++ Calculator2/Calculator.vhd
@@ -4,24 +4,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_signed.all;
 
 
-entity ttt is
+entity calculator is
 PORT(k:in std_logic_vector(3 downto 0);
 		clk, reset: in std_logic;
 		seg4, seg3, seg2, seg1, seg0: out std_logic_vector(6 downto 0);
 		minus: out std_logic);
-end ttt;
+end calculator;
 
-architecture Behavioral of ttt is
+architecture Behavioral of calculator is
 type state is(rst,st0, st1, w1, st2, w2, st3, w3, st4, w4, st5, w5, result, wr1, wr2);
 signal pr_state: state := rst;
 signal nx_state: state;
-
------------------------------------------------
-signal num_s: integer range 0 to 99999 := 0;
-signal num1_s: integer range -99999 to 99999 := 0;
-signal num2_s: integer range -99999 to 99999 := 0;
-signal a_s: integer range -99999 to 99999 := 0; 
-signal flag_s: integer range 0 to 5 := 0;
 signal seg4_s, seg3_s, seg2_s, seg1_s, seg0_s: integer range 0 to 9;
 
 -----------------------------------------------
@@ -35,16 +28,16 @@ end process;
 
 process(pr_state,k)
 variable num: integer range 0 to 99999 := 0;
-variable num1: integer range -99999 to 99999 := 0;
-variable num2: integer range -99999 to 99999 := 0;
+variable num1: integer range 0 to 99999 := 0;
+variable num2: integer range 0 to 99999 := 0;
+variable answer: integer range -99999 to 99999 := 0;
 variable flag: integer range 0 to 5 := 0;
-variable a: integer range -99999 to 99999 := 0;
-
 
 begin
 case pr_state is
 ----------------------------------------------------------------------------------
 when st0 => 
+minus <= '0';
 if(conv_integer(k) >=0 and conv_integer(k)<10) then 
 nx_state <= st1;			
 elsif(k="1010") then 	-- +
@@ -76,18 +69,7 @@ else nx_state <= w1;
 end if;
 ----------------------------------------------------------------------------------
 when st2 => 
---num2 := num1;
 num1 := num;
-
-if(flag=1) then 
-num2:= num1 + num2;
-elsif(flag=2) then 
-num2:= num2 - num1;
-elsif(flag=3) then 
-num2:= num1 * num2;
-elsif(flag=4) then 
-num2:= num2 / num1;
-end if;
 num:=0;
 flag :=1;
 nx_state <= w2;
@@ -98,18 +80,7 @@ else nx_state <= w2;
 end if;
 ----------------------------------------------------------------------------------
 when st3 => 
---num2 := num1;
 num1 := num;
-
-if(flag=1) then 
-num2:= num1 + num2;
-elsif(flag=2) then 
-num2:= num2 - num1;
-elsif(flag=3) then 
-num2:= num1 * num2;
-elsif(flag=4) then 
-num2:= num2 / num1;
-end if;
 num:=0;
 flag :=2;
 nx_state <= w3;
@@ -120,18 +91,7 @@ else nx_state <= w3;
 end if;
 ----------------------------------------------------------------------------------
 when st4 => 
---num2 := num1;
 num1 := num;
-
-if(flag=1) then 
-num2:= num1 + num2;
-elsif(flag=2) then 
-num2:= num2 - num1;
-elsif(flag=3) then 
-num2:= num1 * num2;
-elsif(flag=4) then 
-num2:= num2 / num1;
-end if;
 num:=0;
 flag :=3;
 nx_state <= w2;
@@ -142,18 +102,7 @@ else nx_state <= w4;
 end if;
 ----------------------------------------------------------------------------------
 when st5 => 
---num2 := num1;
 num1 := num;
-
-if(flag=1) then 
-num2:= num1 + num2;
-elsif(flag=2) then 
-num2:= num2 - num1;
-elsif(flag=3) then 
-num2:= num1 * num2;
-elsif(flag=4) then 
-num2:= num2 / num1;
-end if;
 num:=0;
 flag :=4;
 nx_state <= w5;
@@ -167,31 +116,31 @@ when result =>
 num2 := num1;
 num1 := num;
 if(flag=1) then 
-num2:= num1 + num2;
+answer:= num1 + num2;
 elsif(flag=2) then 
-num2:= num2 - num1;
+answer:= num2 - num1;
 elsif(flag=3) then 
-num2:= num1 * num2;
+answer:= num1 * num2;
 elsif(flag=4) then 
-num2:= num2 / num1;
+answer:= num2 / num1;
 end if;
-seg4_s <= (abs(num2) / 10000) mod 10;
-seg3_s <= (abs(num2) / 1000) mod 10;
-seg2_s <= (abs(num2) / 100) mod 10;
-seg1_s <= (abs(num2) / 10) mod 10;
-seg0_s <= (abs(num2) / 1) mod 10;
-if(num2 < 0) then minus <= '1';
+seg4_s <= (abs(answer) / 10000) mod 10;
+seg3_s <= (abs(answer) / 1000) mod 10;
+seg2_s <= (abs(answer) / 100) mod 10;
+seg1_s <= (abs(answer) / 10) mod 10;
+seg0_s <= (abs(answer) / 1) mod 10;
+if(answer < 0) then minus <= '1';
 else minus <= '0';
 end if;
 nx_state <= wr1;
 ----------------------------------------------------------------------------------
 when wr1 => 
-if(k/="1111") then nx_state <= wr2;
+if(k="1111") then nx_state <= wr2;
 else nx_state <= wr1;
 end if;
 ----------------------------------------------------------------------------------
 when wr2 => 
-if(k="1111") then nx_state <= rst;
+if(k/="1111") then nx_state <= rst;
 else nx_state <= wr2;
 end if;
 ----------------------------------------------------------------------------------
@@ -203,12 +152,6 @@ flag:=0;
 nx_state <= st0;
 ----------------------------------------------------------------------------------
 end case;
-
-
-num_s <= num;
- num1_s <= num1;
- num2_s <= num2;
- flag_s <=flag;
 
 
 end process;
